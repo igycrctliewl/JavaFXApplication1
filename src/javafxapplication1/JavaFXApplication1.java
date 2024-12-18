3bbe8032f20288d8bc12bbbe876a567af00b5c58
SurfacePro4 <yomikebro@gmail.com>
2024-12-17 21:27:28 -0800

    Today I experimented with a simple custom control combining a textbox and a button. A HBox contains the custom control.  I created two instances of the custom control on my form. Next experiment is to have them do something.

--- src/javafxapplication1/JavaFXApplication1.java
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -3,9 +3,12 @@ package javafxapplication1;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -42,11 +45,20 @@ public class JavaFXApplication1 extends Application {
 			}
 		} );
 
+		CustomControl cc = new CustomControl();
+		cc.setLayoutX(20);
+		cc.setLayoutY(300);
+
+		CustomControl cc2 = new CustomControl( "my second" );
+		cc2.setLayoutX(20);
+		cc2.setLayoutY(350);
+
 		Pane root = new Pane();
 		//StackPane root = new StackPane();
 		root.getChildren().add( btn );
 		root.getChildren().add( btn2 );
-		root.getChildren().addAll(new Rectangle(100,100,Color.BLUE), new Label("Go!"));
+		root.getChildren().add( cc );
+		root.getChildren().add( cc2 );
 
 
 		Scene scene = new Scene( root, 300, 500 );
@@ -66,5 +78,41 @@ public class JavaFXApplication1 extends Application {
 	 */
 	public static void main( String[] args ) {
 		launch( args );
+	}
+
+
+
+
+	public class CustomControl extends HBox {
+		private TextField textField;
+		private Button button;
+
+		private Insets padding = new Insets(0, 3, 0, 3);
+
+		public CustomControl( String customString ) {
+			super(5);
+			createText( customString );
+			createButton( customString );
+			this.getChildren().addAll( this.textField, this.button );
+		}
+
+		public CustomControl() {
+			super(5);
+			createText( "custom" );
+			createButton( "custom" );
+			this.getChildren().addAll( this.textField, this.button );
+		}
+
+		private void createText( String textString ) {
+			this.textField = new TextField();
+			this.textField.setText( textString.concat( " text" ) );
+			//this.textField.setPadding( padding );
+		}
+
+		private void createButton( String textString ) {
+			this.button = new Button();
+			this.button.setText( textString.concat( " button" ) );
+			//this.button.setPadding( padding );
+		}
 	}
 }
